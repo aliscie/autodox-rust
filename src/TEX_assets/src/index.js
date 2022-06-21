c511cfe8c8df7b0bee63d30dc735fd7bb09a0f2c
E=mc2@6.18 <ali.alkaraawi@crowdbotics.com>
2022-06-21 21:23:28 +0300

    Initial commit.

---
+++ src/TEX_assets/src/index.js
@@ -0,0 +1,19 @@
+import { TEX } from "../../declarations/TEX";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await TEX.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
