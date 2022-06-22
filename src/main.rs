0589ddbc908c9d727e92a49d7bbcb4dbaf69011b
E=mc2@6.18 <ali.alkaraawi@crowdbotics.com>
2022-06-22 15:11:34 +0300

    frontend

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <div class="card">
+            <button {onclick}>{ "+1" }</button>
+            <h1>{ *counter }</h1>
+            </div>
+
+
+        <div class="static_card">
+        <h1>{"Card"}</h1>
+        </div>
+        
+        <div class="static_card bg-blur">
+        <h1>{"Card"}</h1>
+        </div>
+
+        <div class="card bg-blur">
+        <h1>{"Card with Gradient Background"}</h1>
+        </div>
+        </div>
+    }
+}
+
+fn main() {
+    yew::start_app::<App>();
+}
